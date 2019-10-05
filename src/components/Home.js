86cb52f859114716f8fd80e94da001aa413b3d80
Noud de Brouwer <noud4@home.nl>
2019-10-05 12:13:25 +0200

    paginated_posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,17 +8,17 @@ import gql from 'graphql-tag'
 const POSTS_PER_PAGE = 4
 
 // const Home = ({ data: { loading, error, posts, postsConnection, networkStatus }, loadMorePosts }) => {
-const Home = ({ data: { loading, error, posts, networkStatus }, loadMorePosts }) => {
+const Home = ({ data: { loading, error, paginated_posts, networkStatus }, loadMorePosts }) => {
   if (error) return <h1>Error fetching posts!</h1>
   //  if (posts && postsConnection) {
-  if (posts) {
+  if (paginated_posts) {
     // const areMorePosts = posts.length < postsConnection.aggregate.count
-    const areMorePosts = posts.length < posts.paginatorInfo.count
+    const areMorePosts = paginated_posts.length < paginated_posts.paginatorInfo.count
     return (
       <section>
         <ul className='Home-ul'>
           {/* {posts.map(post => ( */}
-          {posts.data.map(post => (
+          {paginated_posts.data.map(post => (
             <li className='Home-li' key={`post-${post.id}`}>
               <Link to={`/post/${post.id}`} className='Home-link'>
                 <div className='Home-placeholder'>
@@ -47,8 +47,8 @@ const Home = ({ data: { loading, error, posts, networkStatus }, loadMorePosts })
 }
 
 export const posts = gql`
-  query posts($first: Int!, $page: Int) {
-    posts(first: $first, page: $page) {
+  query paginated_posts($first: Int!, $page: Int) {
+    paginated_posts(first: $first, page: $page) {
       data{
         id
         slug
@@ -102,7 +102,7 @@ export default graphql(posts, {
     loadMorePosts: () => {
       return data.fetchMore({
         variables: {
-          page: data.posts.length
+          page: data.paginated_posts.length
           // skip: data.posts.length
         },
         updateQuery: (previousResult, { fetchMoreResult }) => {
@@ -111,7 +111,7 @@ export default graphql(posts, {
           }
           return Object.assign({}, previousResult, {
             // Append the new posts results to the old one
-            posts: [...previousResult.posts, ...fetchMoreResult.posts]
+            posts: [...previousResult.paginated_posts, ...fetchMoreResult.paginated_posts]
           })
         }
       })
